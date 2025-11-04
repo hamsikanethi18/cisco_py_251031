a39d64e1cd4d58aff8aa8958879ab369d3e98779
hamsikanethi18 <nethihamsika@gmail.com>
2025-11-04 08:44:51 +0530

    List and tuple with file Handling

--- days 251031/app/qn5_sol.py
+++ days 251031/app/qn5_sol.py
@@ -1,9 +1,24 @@
 
 
-numbers_input=input("Enter a sentence of numbers separated by spaces: ")
-numbers_list=[]
-for num in numbers_input.split():
-    numbers_list.append(int(num))
-min_num=min(numbers_list)
-max_num=max(numbers_list)
-
+numbers=input("Enter sequence of numbers : ")
+list_numbers=numbers.split()
+new_list=[]
+for nums in list_numbers:
+    nums=int(nums)
+    new_list.append(nums)
+min_val=new_list[0]
+max_val=new_list[0]
+for integer in new_list:
+    if integer<min_val:
+        min_val=integer
+    if integer>max_val:
+        max_val=integer
+print(min_val)
+print(max_val)
+with open("minmax_data.txt","w") as output_file:
+    output_file.write(f"List:{new_list}\n")
+    output_file.write(f"Minimum:{min_val}\n")
+    output_file.write(f"Maximum:{max_val}\n")
+with open("minmax_data.txt","r") as input_file:
+    file=input_file.read()
+    print(file)
